41cad0affb6b3b0e60964f72099279dad9531b4d
huskiefu <pzyleague@Gmail.com>
2022-03-10 00:49:24 +0800

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navibar = () => {
 
   return(
     <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home" className = "navbar-brand">Navbar</Navbar.Brand>
+    <Navbar.Brand href="#home" className = "navbar-brand">My Blog</Navbar.Brand>
     <Nav className="me-auto">
     <Nav.Link as={Link} to = "/">Home</Nav.Link>
     <Nav.Link as={Link} to = "/compose">Compose</Nav.Link>
