af3ce91614b169f9fa551266886a2775629898dd
huskiefu <pzyleague@Gmail.com>
2022-03-10 00:33:00 +0800

    remove test from scripts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
+import Home from "./components/Home/Home";
+import Compose from "./components/Compose/Compose";
+import Navibar from "./components/Navbar";
+import Post from "./components/Post/Post";
+import Axios from "axios";
+import {AuthProvider} from "./contexts/AuthContext"
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
+import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes"
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  async function retrievePosts() {
+    const {data} = await Axios.get("http://localhost:5000");
+    setPosts(data);
+  }
+
+  function submitPost(inputTitle, inputContent){
+    Axios.post('http://localhost:5000', {
+      title : inputTitle,
+      content : inputContent
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  useEffect(()=> {
+    retrievePosts();
+  },[posts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <AuthProvider>
+    <Navibar/>
+        <Routes>
+          <Route exact path = "/signup" element = {<Signup/>}/>
+          <Route exact path = "/login" element = {<Login/>}/>
+          <Route element = {<ProtectedRoutes/>}>
+          <Route exact path = "/" element = {<Home posts = {posts}/>}/>
+          <Route exact path = "/compose" element = {<Compose submitPost = {submitPost}/>}/>
+          <Route exact path="/posts/:id" element = {<Post retrievePosts = {retrievePosts}/>}/>
+          </Route>
+        </Routes>
+    </AuthProvider>
+    </Router>
   );
 }
 
